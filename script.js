48160dfbdb8a73500573b50d5e6a8c48a8057566
pancho040 <molinafranz943@gmail.com>
2025-05-14 11:59:31 -0400

    Primer commit: subir proyecto

---
+++ script.js
@@ -0,0 +1,4 @@
+// Script para cambiar el mensaje cuando se hace clic en el botón
+document.getElementById('clickButton').addEventListener('click', function() {
+  document.getElementById('message').textContent = '¡Has hecho clic en el botón!';
+});
